e04e15384047cd891091bc1730e1c6c1d4162ca4
olenazamyslova <89514176+olenazamyslova@users.noreply.github.com>
2023-01-31 07:25:06 +0000

    Deploying to gh-pages from @ olenazamyslova/goit-js-hw-09@ca485f4cd6f6880929472638370429c21bc32cc3 🚀

---
+++ 01-color-switcher.71410c8c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),d=null;n.disabled=!0,e.addEventListener("click",(function(){d=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.disabled=!0,n.disabled=!1})),n.addEventListener("click",(function(){clearInterval(d),e.disabled=!1,n.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.71410c8c.js.map
